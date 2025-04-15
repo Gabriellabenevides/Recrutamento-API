1abbadd8bfcb4596037fa19d03b405c37d3e0d7d
gabi <gabriellabenevides22@gmail.com>
2025-04-15 16:45:37 -0300

    Criação do projeto RecrutamentoAPI com EF Core 8.0.13
    Implementação da API RecrutamentoAPI utilizando .NET 9.0 e Entity Framework Core 8.0.13. Adição do arquivo de solução e configuração dos projetos `Recrutamento.Application`, `Recrutamento.Domain` e `Recrutamento.MySQL`.
    
    Configuração dos arquivos `.csproj` para os projetos, incluindo referências a pacotes necessários e arquivos de configuração `appsettings.json` para logging e conexão com o banco de dados.
    
    Criação de classes de migração para definir a estrutura das tabelas `Candidatos`, `Vagas` e `ProcessoSeletivos` no MySQL, além da definição do contexto do banco de dados em `MySQLContext.cs`.
    
    Implementação da configuração do Swagger para documentação da API e definição das entidades `Candidato`, `ProcessoSeletivo` e `Vaga` com seus respectivos relacionamentos.
    
    Configuração do arquivo `Program.cs` para inicialização da aplicação e definição do pipeline de requisições, além da adição do arquivo `launchSettings.json` para as configurações de desenvolvimento.

---
+++ RecrutamentoAPI/Recrutamento.MySQL/MySQL/MySQLContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recrutamento.Domain.Entities;
+
+namespace Recrutamento.MySQL.MySQL;
+
+public class MySQLContext : DbContext
+{
+    public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+    {
+    }
+    public DbSet<Candidato> Candidatos { get; set; }
+    public DbSet<ProcessoSeletivo> ProcessoSeletivos { get; set; }
+    public DbSet<Vaga> Vagas { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure your entity mappings here
+        // For example:
+        // modelBuilder.Entity<YourEntity>().ToTable("YourTableName");
+    }
+}
---
+++ RecrutamentoAPI/Recutamento.Domain/Entities/Vaga.cs
@@ -0,0 +1,12 @@
+﻿namespace Recrutamento.Domain.Entities
+{
+    public class Vaga
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Localizacao { get; set; }
+
+        public ICollection<ProcessoSeletivo> ProcessosSeletivos { get; set; }
+    }
+}
